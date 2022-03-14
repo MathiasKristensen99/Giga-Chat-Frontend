8932cb35e07c8f120afdb2dcd717e91812707826
Mathias Kristensen <mathiaskristensen52@gmail.com>
2022-03-14 12:04:48 +0100

    Update FindUserView.vue

--- src/views/FindUserView.vue
+++ src/views/FindUserView.vue
@@ -1,6 +1,6 @@
 <template>
   <h2>Search for user:</h2>
-  <input v-model="inputName" placeholder="Enter name">
+  <input v-model="inputName" placeholder="Enter name" />
   <button type="submit" @click="searchUser"></button>
   <p v-if="userStore.userFound.length > 0">
     | User found: {{ userStore.userFound }}
@@ -15,7 +15,6 @@ const userStore = UserStore();
 const inputName = ref("");
 
 function searchUser() {
-  console.log(userStore.userFound);
   userStore.searchUser(inputName.value);
 }
 </script>
